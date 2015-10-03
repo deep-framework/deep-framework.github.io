8e9f1596ae888f356f5ae08d427f574377556a97
AlexanderC <alexander.moldova@gmail.com>
2015-10-03 16:31:50 -0700

    pickup docs

--- pickup.sh
+++ pickup.sh
@@ -8,3 +8,23 @@ if [ -z $1 ]; then
 fi
 
 cp -R $1 ${path}
+
+libs_file=${path}/libs.json
+put_comma=false
+
+echo -n '{' > ${libs_file}
+
+for lib in ${path}/*; do
+  if [ -d ${lib} ]; then
+    lib_name=$(basename ${lib})
+
+    if [ ${put_comma} == true ]; then
+      echo -n ',' >> ${libs_file}
+    fi
+
+    echo -n '"'${lib_name}'":"'${lib_name}'"' >> ${libs_file}
+    put_comma=true
+  fi
+done
+
+echo -n '}' >> ${libs_file}

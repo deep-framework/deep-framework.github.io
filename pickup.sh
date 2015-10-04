1cc3bd6ffed0387f20b32ceddf828493dd54fed2
AlexanderC <alexander.moldova@gmail.com>
2015-10-03 17:38:23 -0700

    add global badge

--- pickup.sh
+++ pickup.sh
@@ -10,13 +10,24 @@ fi
 cp -R $1 ${path}
 
 libs_file=${path}/libs.json
+badge_tpl_file=${path}/badge.svg.tpl
+badge_file=${path}/badge.svg
 put_comma=false
+total_coverage=0
+cov_num=0
 
 echo -n '{' > ${libs_file}
 
 for lib in ${path}/*; do
   if [ -d ${lib} ]; then
+    lib_coverage=$(cat ${lib}/coverage.json | grep coverage | awk -F': "' '{print $2;}' | awk -F'%' '{print $1;}')
+
+    cov_num=$(echo "$cov_num+1" | bc)
+    total_coverage=$(echo "$total_coverage+$lib_coverage" | bc)
+
     lib_name=$(basename ${lib})
+
+    echo 'Library '${lib_name}' found with coverage '${lib_coverage}'%'
 
     if [ ${put_comma} == true ]; then
       echo -n ',' >> ${libs_file}
@@ -28,3 +39,11 @@ for lib in ${path}/*; do
 done
 
 echo -n '}' >> ${libs_file}
+
+total_coverage=$(echo "$total_coverage/$cov_num" | bc)
+
+echo 'Total coverage '${total_coverage}'%'
+
+badge_content=$(cat ${badge_tpl_file} | sed "s/{perc}/${total_coverage}/g")
+
+echo ${badge_content} > ${badge_file}
